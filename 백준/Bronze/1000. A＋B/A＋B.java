e91e5373a65ab80d9d4b52a41d3416dbe74ade46
tuioe <qodlgks123@naver.com>
2024-01-12 19:19:29 +0900

    [Bronze V] Title: A+B, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a,b = 0;
+		String[] s = br.readLine().split(" ");
+		
+		a = Integer.parseInt(s[0]);
+		b = Integer.parseInt(s[1]);
+		
+		System.out.println(a+b);
+	}
+}
