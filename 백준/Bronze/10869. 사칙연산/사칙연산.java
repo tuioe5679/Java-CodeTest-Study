43e7aedb2ca81b750f21a5aadfa17de6804562d4
tuioe <qodlgks123@naver.com>
2024-03-11 15:39:58 +0900

    [Bronze V] Title: 사칙연산, Time: 132 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a,b = 0;
+        String[] s = br.readLine().split(" ");
+
+        a = Integer.parseInt(s[0]);
+        b = Integer.parseInt(s[1]);
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
