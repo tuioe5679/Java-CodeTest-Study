f1f7e9ce66b662bfa5529872ff59ff556a64f39e
tuioe <qodlgks123@naver.com>
2024-03-13 19:18:40 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -1,9 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main{
-    public static void main(String[]args){
-        Scanner stdin = new Scanner(System.in);
-        String as = stdin.next();
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String as = br.readLine();
         System.out.println(as.length());
     }    
 }
